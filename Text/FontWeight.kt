3ecf800efb3969cc994c37005f4d9f100936b709
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-26 16:03:50 -0800

    Create FontWeight.kt

---
+++ Text/FontWeight.kt
@@ -0,0 +1,126 @@
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun Examples() {
+
+    // FontWeight Thin
+    Text(
+        text = "Thin FontWeight",
+        fontWeight = FontWeight.Thin
+    )
+    
+    
+    // FontWeight ExtraLight
+    Text(
+        text = "ExtraLight FontWeight",
+        fontWeight = FontWeight.ExtraLight
+    )
+    
+    
+    // FontWeight Light
+    Text(
+        text = "Light FontWeight",
+        fontWeight = FontWeight.Light
+    )
+    
+    
+    // FontWeight Normal
+    Text(
+        text = "Normal FontWeight",
+        fontWeight = FontWeight.Normal
+    )
+    
+    
+    // FontWeight SemiBold
+    Text(
+        text = "SemiBold FontWeight",
+        fontWeight = FontWeight.SemiBold
+    )
+    
+    
+    // FontWeight Bold
+    Text(
+        text = "Bold FontWeight",
+        fontWeight = FontWeight.Bold
+    )
+    
+    
+    // FontWeight ExtraBold
+    Text(
+        text = "ExtraBold FontWeight",
+        fontWeight = FontWeight.ExtraBold
+    )
+    
+    
+    // FontWeight Black
+    Text(
+        text = "Black FontWeight",
+        fontWeight = FontWeight.Black
+    )
+    
+    
+    // FontWeight W100
+    Text(
+        text = "W100 FontWeight",
+        fontWeight = FontWeight.W100
+    )
+    
+    
+    // FontWeight W200
+    Text(
+        text = "W200 FontWeight",
+        fontWeight = FontWeight.W200
+    )
+    
+    
+    // FontWeight W300
+    Text(
+        text = "W300 FontWeight",
+        fontWeight = FontWeight.W300
+    )
+    
+    
+    // FontWeight W400
+    Text(
+        text = "W400 FontWeight",
+        fontWeight = FontWeight.W400
+    )
+    
+    
+    // FontWeight W500
+    Text(
+        text = "W500 FontWeight",
+        fontWeight = FontWeight.W500
+    )
+    
+    
+    // FontWeight W600
+    Text(
+        text = "W600 FontWeight",
+        fontWeight = FontWeight.W600
+    )
+    
+    
+    // FontWeight W700
+    Text(
+        text = "W700 FontWeight",
+        fontWeight = FontWeight.W700
+    )
+    
+    
+    // FontWeight W800
+    Text(
+        text = "W800 FontWeight",
+        fontWeight = FontWeight.W800
+    )
+    
+    
+    // FontWeight W900
+    Text(
+        text = "W900 FontWeight",
+        fontWeight = FontWeight.W900
+    )
+    
+}
