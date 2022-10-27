b40275d09f054746ccf9ebb2f661a62db82e5230
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-26 16:00:32 -0800

    Create FontSize.kt

---
+++ Text/FontSize.kt
@@ -0,0 +1,112 @@
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Examples() {
+
+    // FontSize Display Large
+    Text(
+        text = "displayLarge",
+        fontSize = MaterialTheme.typography.displayLarge.fontSize
+    )
+
+
+    // FontSize Display Medium
+    Text(
+        text = "displayMedium",
+        fontSize = MaterialTheme.typography.displayMedium.fontSize
+    )
+    
+
+    // FontSize Display Small
+    Text(
+        text = "displaySmall",
+        fontSize = MaterialTheme.typography.displaySmall.fontSize
+    )
+
+
+    // FontSize Headline Large
+    Text(
+        text = "headlineLarge",
+        fontSize = MaterialTheme.typography.headlineLarge.fontSize
+    )
+
+
+    // FontSize Headline Medium
+    Text(
+        text = "headlineMedium",
+        fontSize = MaterialTheme.typography.headlineMedium.fontSize
+    )
+
+
+    // FontSize Headline Small
+    Text(
+        text = "headlineSmall",
+        fontSize = MaterialTheme.typography.headlineSmall.fontSize
+    )
+
+
+    // FontSize Title Large
+    Text(
+        text = "titleLarge",
+        fontSize = MaterialTheme.typography.titleLarge.fontSize
+    )
+
+
+    // FontSize Title Medium
+    Text(
+        text = "titleMedium",
+        fontSize = MaterialTheme.typography.titleMedium.fontSize
+    )
+
+
+    // FontSize Title Small
+    Text(
+        text = "titleSmall",
+        fontSize = MaterialTheme.typography.titleSmall.fontSize
+    )
+
+
+    // FontSize Body Large
+    Text(
+        text = "bodyLarge",
+        fontSize = MaterialTheme.typography.bodyLarge.fontSize
+    )
+
+
+    // FontSize Body Medium
+    Text(
+        text = "bodyMedium",
+        fontSize = MaterialTheme.typography.bodyMedium.fontSize
+    )
+
+
+    // FontSize Body Small
+    Text(
+        text = "bodySmall",
+        fontSize = MaterialTheme.typography.bodySmall.fontSize
+    )
+
+
+    // FontSize Label Large
+    Text(
+        text = "labelLarge",
+        fontSize = MaterialTheme.typography.labelLarge.fontSize
+    )
+
+
+    // FontSize Label Medium
+    Text(
+        text = "labelMedium",
+        fontSize = MaterialTheme.typography.labelMedium.fontSize
+    )
+
+
+    // FontSize Label Small
+    Text(
+        text = "labelSmall",
+        fontSize = MaterialTheme.typography.labelSmall.fontSize
+    )
+
+}
