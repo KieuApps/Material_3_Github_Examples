ee3e3efc75d8bb81386ede5439acb1ede630ee30
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-27 06:42:34 -0800

    Update DismissibleNavigationDrawer.kt

--- NavigationDrawer/DismissibleNavigationDrawer.kt
+++ NavigationDrawer/DismissibleNavigationDrawer.kt
@@ -18,57 +18,57 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DismissibleDrawerExample() {
-  Box(modifier = Modifier.height(250.dp)) {
+    Box(modifier = Modifier.height(250.dp)) {
 
-    val scope = rememberCoroutineScope()
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val tools = listOf(
-      "Drag" to painterResource(id = drawable.ic_baseline_pan_tool_alt_24),
-      "Pen" to painterResource(id = drawable.ic_baseline_draw_24),
-      "Pencil" to painterResource(id = drawable.ic_baseline_mode_edit_outline_24),
-    )
-    var selected by remember { mutableStateOf(0) }
+        val scope = rememberCoroutineScope()
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val tools = listOf(
+            "Drag" to painterResource(id = drawable.ic_baseline_pan_tool_alt_24),
+            "Pen" to painterResource(id = drawable.ic_baseline_draw_24),
+            "Pencil" to painterResource(id = drawable.ic_baseline_mode_edit_outline_24),
+        )
+        var selected by remember { mutableStateOf(0) }
 
-    // Close the drawer on back press
-    BackHandler(enabled = drawerState.isOpen) {
-      scope.launch { drawerState.close() }
-    }
-
-    // Menu button
-    FilledTonalIconButton(
-      onClick = { scope.launch { drawerState.open() } },
-      modifier = Modifier.padding(16.dp)
-    ) {
-      Icon(
-        painter = tools[selected].second,
-        contentDescription = tools[selected].first
-      )
-    }
-
-    // Drawer
-    DismissibleNavigationDrawer(
-      drawerState = drawerState,
-      drawerContent = {
-        DismissibleDrawerSheet {
-          tools.forEachIndexed { index, tool ->
-            NavigationDrawerItem(
-              label = { Text(tool.first) },
-              selected = selected == index,
-              onClick = {
-                selected = index
-                scope.launch { drawerState.close() }
-              },
-              icon = { Icon(tool.second, tool.first) },
-              modifier = Modifier.padding(8.dp)
-            )
-          }
+        // Close the drawer on back press
+        BackHandler(enabled = drawerState.isOpen) {
+            scope.launch { drawerState.close() }
         }
-      }
-    ) {
-      // Your main content...
 
+        // Menu button
+        FilledTonalIconButton(
+            onClick = { scope.launch { drawerState.open() } },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Icon(
+                painter = tools[selected].second,
+                contentDescription = tools[selected].first
+            )
+        }
+
+        // Drawer
+        DismissibleNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                DismissibleDrawerSheet {
+                    tools.forEachIndexed { index, tool ->
+                        NavigationDrawerItem(
+                            label = { Text(tool.first) },
+                            selected = selected == index,
+                            onClick = {
+                                selected = index
+                                scope.launch { drawerState.close() }
+                            },
+                            icon = { Icon(tool.second, tool.first) },
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+                }
+            }
+        ) {
+            // Your main content...
+
+        }
     }
-  }
 }
 
 
@@ -77,73 +77,73 @@ fun DismissibleDrawerExample() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DismissibleDrawerColorsExample() {
-  Box(modifier = Modifier.height(250.dp)) {
-    
-    val lightBlue = Color(0xFF55A29A).copy(0.72f)
-    val orange = Color(0xFFFFDFAE).copy(0.72f)
-    val purple = Color(0xFF9DD5CF).copy(0.72f)
-    val scope = rememberCoroutineScope()
-    val drawerState = rememberDrawerState(initialValue = Closed)
-    val tools = listOf(
-      "Drag" to painterResource(id = drawable.ic_baseline_pan_tool_alt_24),
-      "Pen" to painterResource(id = drawable.ic_baseline_draw_24),
-      "Pencil" to painterResource(id = drawable.ic_baseline_mode_edit_outline_24),
-    )
-    var selected by remember { mutableStateOf(0) }
+    Box(modifier = Modifier.height(250.dp)) {
 
-    // Close the drawer on back press
-    BackHandler(enabled = drawerState.isOpen) {
-      scope.launch { drawerState.close() }
-    }
+        val lightBlue = Color(0xFF55A29A).copy(0.72f)
+        val orange = Color(0xFFFFDFAE).copy(0.72f)
+        val purple = Color(0xFF9DD5CF).copy(0.72f)
+        val scope = rememberCoroutineScope()
+        val drawerState = rememberDrawerState(initialValue = Closed)
+        val tools = listOf(
+            "Drag" to painterResource(id = drawable.ic_baseline_pan_tool_alt_24),
+            "Pen" to painterResource(id = drawable.ic_baseline_draw_24),
+            "Pencil" to painterResource(id = drawable.ic_baseline_mode_edit_outline_24),
+        )
+        var selected by remember { mutableStateOf(0) }
 
-    // Menu button
-    FilledTonalIconButton(
-      onClick = { scope.launch { drawerState.open() } },
-      modifier = Modifier.padding(16.dp)
-    ) {
-      Icon(
-        painter = tools[selected].second,
-        contentDescription = tools[selected].first
-      )
-    }
-
-    // Drawer
-    DismissibleNavigationDrawer(
-      drawerState = drawerState,
-      drawerContent = {
-        DismissibleDrawerSheet(
-          drawerShape = RoundedCornerShape(32.dp),
-          drawerContainerColor = lightBlue,
-          drawerContentColor = Color.Black,
-          drawerTonalElevation = 8.dp
-        ) {
-          tools.forEachIndexed { index, tool ->
-            NavigationDrawerItem(
-              label = { Text(tool.first) },
-              selected = selected == index,
-              onClick = {
-                selected = index
-                scope.launch { drawerState.close() }
-              },
-              icon = { Icon(tool.second, tool.first) },
-              modifier = Modifier.padding(8.dp),
-              colors = NavigationDrawerItemDefaults.colors(
-                selectedContainerColor = orange,
-                unselectedContainerColor = purple,
-                selectedIconColor = Color.Black,
-                unselectedIconColor = Color.DarkGray,
-                selectedTextColor = Color.Black,
-                unselectedTextColor = Color.DarkGray,
-                selectedBadgeColor = Color.Black,
-                unselectedBadgeColor = Color.DarkGray
-              )
-            )
-          }
+        // Close the drawer on back press
+        BackHandler(enabled = drawerState.isOpen) {
+            scope.launch { drawerState.close() }
         }
-      }
-    ) {
-      // Your main content...
 
+        // Menu button
+        FilledTonalIconButton(
+            onClick = { scope.launch { drawerState.open() } },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Icon(
+                painter = tools[selected].second,
+                contentDescription = tools[selected].first
+            )
+        }
+
+        // Drawer
+        DismissibleNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                DismissibleDrawerSheet(
+                    drawerShape = RoundedCornerShape(32.dp),
+                    drawerContainerColor = lightBlue,
+                    drawerContentColor = Color.Black,
+                    drawerTonalElevation = 8.dp
+                ) {
+                    tools.forEachIndexed { index, tool ->
+                        NavigationDrawerItem(
+                            label = { Text(tool.first) },
+                            selected = selected == index,
+                            onClick = {
+                                selected = index
+                                scope.launch { drawerState.close() }
+                            },
+                            icon = { Icon(tool.second, tool.first) },
+                            modifier = Modifier.padding(8.dp),
+                            colors = NavigationDrawerItemDefaults.colors(
+                                selectedContainerColor = orange,
+                                unselectedContainerColor = purple,
+                                selectedIconColor = Color.Black,
+                                unselectedIconColor = Color.DarkGray,
+                                selectedTextColor = Color.Black,
+                                unselectedTextColor = Color.DarkGray,
+                                selectedBadgeColor = Color.Black,
+                                unselectedBadgeColor = Color.DarkGray
+                            )
+                        )
+                    }
+                }
+            }
+        ) {
+            // Your main content...
+
+        }
     }
-  }
 }
