880204e2de72fbc74e8a4f9578b21f717a80e282
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-27 06:41:32 -0800

    Update PermanentNavigationDrawer.kt

--- NavigationDrawer/PermanentNavigationDrawer.kt
+++ NavigationDrawer/PermanentNavigationDrawer.kt
@@ -1,5 +1,6 @@
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Delete
@@ -7,13 +8,14 @@ import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
+
+// Permanent Drawer
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Examples() {
-
-    // PermanentNavigationDrawer
+fun PermanentDrawerExample() {
     val pages = listOf(
         "Create" to Icons.Default.Create,
         "Inbox" to Icons.Default.Email,
@@ -42,5 +44,56 @@ fun Examples() {
         // Your main content...
 
     }
+}
 
+
+// Permanent Drawer Colors
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PermanentDrawerColorsExample() {
+    
+    val lightBlue = Color(0xFF55A29A)
+    val orange = Color(0xFFFFDFAE)
+    val purple = Color(0xFF9DD5CF)
+    val pages = listOf(
+        "Create" to Icons.Default.Create,
+        "Inbox" to Icons.Default.Email,
+        "Trash" to Icons.Default.Delete
+    )
+    var selected by remember { mutableStateOf(0) }
+
+    PermanentNavigationDrawer(
+        drawerContent = {
+            PermanentDrawerSheet(
+                modifier = Modifier.fillMaxWidth(0.5f),
+                drawerShape = RoundedCornerShape(32.dp),
+                drawerContainerColor = lightBlue,
+                drawerContentColor = Color.Black,
+                drawerTonalElevation = 8.dp
+            ) {
+                pages.forEachIndexed { index, page ->
+                    NavigationDrawerItem(
+                        label = { Text(page.first) },
+                        selected = selected == index,
+                        onClick = { selected = index },
+                        icon = { Icon(page.second, page.first) },
+                        modifier = Modifier.padding(8.dp),
+                        colors = NavigationDrawerItemDefaults.colors(
+                            selectedContainerColor = orange,
+                            unselectedContainerColor = purple,
+                            selectedIconColor = Color.Black,
+                            unselectedIconColor = Color.DarkGray,
+                            selectedTextColor = Color.Black,
+                            unselectedTextColor = Color.DarkGray,
+                            selectedBadgeColor = Color.Black,
+                            unselectedBadgeColor = Color.DarkGray
+                        )
+                    )
+                }
+            }
+        }
+    ) {
+        // Your main content...
+
+    }
 }
