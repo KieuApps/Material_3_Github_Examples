ff4af43b672a3ec64a181c3d63f93db041d1316d
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-28 06:28:25 -0800

    Update AlertDialog.kt

--- Dialog/AlertDialog.kt
+++ Dialog/AlertDialog.kt
@@ -4,28 +4,68 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Examples() {
-    
-    // AlertDialog
+
     var showAlert by remember { mutableStateOf(false) }
 
-    TextButton({ showAlert = true }) {
+
+    // AlertDialog
+    Button({ showAlert = true }) {
         Text("Click for preview")
     }
-    
+
     AnimatedVisibility(showAlert) {
         AlertDialog(
             onDismissRequest = { showAlert = false },
             title = { Text("Alert") },
             text = { Text("You clicked the button.") },
             confirmButton = {
-                Button({ showAlert = false }) { Text("Confirm") }
+                Button({ showAlert = false }) {
+                    Text("Confirm")
+                }
             },
             dismissButton = {
-                TextButton({ showAlert = false }) { Text("Dismiss") }
+                TextButton({ showAlert = false }) {
+                    Text("Dismiss")
+                }
             }
+        )
+    }
+
+
+    // AlertDialog Colors
+    val containerColor = Color(0xFF5E4463)
+
+    Button({ showAlert = true }) {
+        Text("Click for preview (AlertDialog Colors)")
+    }
+
+    AnimatedVisibility(showAlert) {
+        AlertDialog(
+            onDismissRequest = { showAlert = false },
+            title = { Text("Alert") },
+            text = { Text("You clicked the button.") },
+            confirmButton = {
+                Button({ showAlert = false }) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton({ showAlert = false }) {
+                    Text("Dismiss")
+                }
+            },
+            shape = RectangleShape,
+            containerColor = containerColor,
+            iconContentColor = Color.White,
+            titleContentColor = Color.White,
+            textContentColor = Color.White,
+            tonalElevation = 8.dp
         )
     }
 
