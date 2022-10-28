7de11ec234caef935355956cc45885a06918e4c2
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-28 04:42:05 -0800

    Update ExposedDropdownMenu.kt

--- DropdownMenu/ExposedDropdownMenu.kt
+++ DropdownMenu/ExposedDropdownMenu.kt
@@ -2,16 +2,18 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Examples() {
 
-    // ExposedDropdownMenu
     val items = listOf("Item 1", "Item 2", "Item 3", "Item 4")
     var expanded by remember { mutableStateOf(false) }
     var selected by rememberSaveable { mutableStateOf(items[0]) }
 
+
+    // ExposedDropdownMenu
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = { expanded = !expanded }
@@ -45,4 +47,55 @@ fun Examples() {
         }
     }
 
+
+    // ExposedDropdownMenu Colors
+    val contentColor = Color(0xFF6E3CC5)
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded }
+    ) {
+        TextField(
+            readOnly = true,
+            value = selected,
+            onValueChange = { },
+            label = { Text("Dropdown Colors") },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            modifier = Modifier.menuAnchor(),
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.LightGray,
+                containerColor = Color.Black,
+                cursorColor = contentColor,
+                focusedIndicatorColor = contentColor,
+                unfocusedIndicatorColor = Color.Gray,
+                focusedTrailingIconColor = contentColor,
+                unfocusedTrailingIconColor = Color.Gray,
+                focusedLabelColor = contentColor,
+                unfocusedLabelColor = Color.Gray
+            )
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            items.forEach { item ->
+                DropdownMenuItem(
+                    text = { Text(item) },
+                    onClick = {
+                        selected = item
+                        expanded = false
+                    },
+                    colors = MenuDefaults.itemColors(
+                        textColor = contentColor,
+                        leadingIconColor = contentColor,
+                        trailingIconColor = contentColor,
+                    )
+                )
+            }
+        }
+    }
 }
